c3ec50b3da8dda215fa539acc8163031621e2e99
Bruno Ciconelle <bciconel@redhat.com>
2022-03-15 21:23:20 -0300

    wip

--- setup.py
+++ setup.py
@@ -13,14 +13,13 @@ setup(
         "Programming Language :: Python :: 3.9",
     ],
     license="GPLv3+",
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     # Runtime dependencies are not included here. Only setuptools-git-versioning for proper version resolution.
     # Check requirements.txt and dev-requirements.txt for production and development dependencies
     setup_requires=["setuptools-git-versioning"],
     setuptools_git_versioning={
         "enabled": True,
         "dev_template": "{tag}.dev{ccount}",
-        "dirty_template": "{tag}.dev{ccount}a",
         "starting_version": DEFAULT_VERSION,
     },
 )
